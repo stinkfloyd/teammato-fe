28ef66c06b6505086c95440833a728090264242c
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2019-01-02 21:43:25 -0700

    TSLint

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core'
-import { User } from '../user'
 import { formatDate } from '@angular/common'
 import { HttpClient } from '@angular/common/http'
 import { Router } from '@angular/router'

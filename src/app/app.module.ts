9a0a3971038e7856ebe8424151f3c33925129063
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2019-01-10 08:16:44 -0700

    "Bootstrap imports fixed,navbar dropdown added."

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,8 @@ import { SignUpFormComponent } from './sign-up-form/sign-up-form.component'
 import { LogInFormComponent } from './log-in-form/log-in-form.component'
 import { ProfileComponent } from './profile/profile.component'
 import { CreateTeamComponent } from './create-team/create-team.component'
-import { CreatedTeamsComponent } from './created-teams/created-teams.component';
-import { JoinedTeamsComponent } from './joined-teams/joined-teams.component';
+import { CreatedTeamsComponent } from './created-teams/created-teams.component'
+import { JoinedTeamsComponent } from './joined-teams/joined-teams.component'
 import { JoinTeamComponent } from './join-team/join-team.component'
 
 @NgModule({
